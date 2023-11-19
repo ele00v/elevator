0dbc2fb88dc1d3acc20ffba80be2627efdc84664
Elena Vera <vera19elena@gmail.com>
2023-11-19 12:27:12 +0200

    Update elevator.py
    create main and find_solution(). add initial_state and goal variables

--- elevator.py
+++ elevator.py
@@ -1,24 +1,35 @@
-def dfs_function():
-    print("DFS FUNCTION")
+def find_solution(initial_state,goal,method):
+    if method=="DFS":
+        print("1.DFS")
+    elif method=="BFS":
+        print("2.BFS")
+    elif method=="--":
+        print("3.---")
 
-def bfs_function():
-    print("BFS FUNCTION")
+def main():
 
-while 1:
-    print("Select between the given searching algorithms:")
-    print("1.DFS\n2.BFS")
-    print("Select option:")
-    option= input()
-    if option=="1" :
-        dfs_function()
-        break
-    elif option=="2" :
-        bfs_function()
-        break
-    else:
-        print("Wrong input")
+    initial_state = [0, 9, 4, 12, 7, 0]
+    goal = [5, 0, 0, 0, 0, 0]
 
+    while 1:
+        print("Select between the given searching algorithms:")
+        print("1.DFS\n2.BFS\n3.---")
+        print("Select option:")
+        option= input()
+        if option=="1" :
+            method="DFS"
+            find_solution(initial_state,goal,method)
+            break
+        elif option=="2":
+            method="BFS"
+            find_solution(initial_state,goal,method)
+            break
+        elif option=="3":
+            method="---"
+            find_solution(initial_state,goal,method)
+            break
+        else:
+            print("Wrong input")
 
-
-
-
+if __name__ == "__main__":
+    main()
